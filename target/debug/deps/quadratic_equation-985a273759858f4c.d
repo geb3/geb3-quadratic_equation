7ce845a600a995640903abdf4c27e1d92c17c681
George <gosha738g@gmail.com>
2022-06-23 18:40:51 +0300

    v.stable

---
+++ target/debug/deps/quadratic_equation-985a273759858f4c.d
@@ -0,0 +1,5 @@
+/home/gosha/Programmer/Rust/quadratic_equation/target/debug/deps/quadratic_equation-985a273759858f4c: src/main.rs
+
+/home/gosha/Programmer/Rust/quadratic_equation/target/debug/deps/quadratic_equation-985a273759858f4c.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/quadratic_equation.d
@@ -0,0 +1,1 @@
+/home/gosha/Programmer/Rust/quadratic_equation/target/debug/quadratic_equation: /home/gosha/Programmer/Rust/quadratic_equation/src/main.rs
